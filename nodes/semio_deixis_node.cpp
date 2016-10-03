064051921b2e5742d0009080db4c82dcd1d2b84d
Edward T. Kaszubski <ekaszubski@gmail.com>
2016-10-02 20:52:46 -0700

    Add HumanoidFilter to semio_deixis_node.

--- nodes/semio_deixis_node.cpp
+++ nodes/semio_deixis_node.cpp
@@ -30,7 +30,15 @@ public:
         targets_sub_( nh_rel_.subscribe( "targets", 10, &SemioDeixisNode::targetsCB, this ) ),
         humanoid_source_ptr_( humanoid_source_ptr )
     {
-        //
+        if( nh_rel.param<bool>( std::string( "filter_humanoids" ), true ) )
+        {
+            humanoid_source_ptr_->getFilter().addFilters( semio::HumanoidFilter::getStandardFilterHumanoidJoint( semio::HumanoidJoint::JointType::HEAD ) );
+            humanoid_source_ptr_->getFilter().addFilter( semio::HumanoidFilter::_FilterFunc( []( semio::Humanoid const & humanoid )->bool{
+                return (
+                    humanoid.getJoint( semio::HumanoidJoint::JointType::LEFT_HAND ).isTracked( semio::HumanoidJoint::CONFIDENCE_LOW ) ||
+                    humanoid.getJoint( semio::HumanoidJoint::JointType::RIGHT_HAND ).isTracked( semio::HumanoidJoint::CONFIDENCE_LOW ) );
+            } ) );
+        }
     }
 
     void spin()
