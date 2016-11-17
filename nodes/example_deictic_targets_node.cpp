4c6df48cb35a8bfe086107361fc1b3edfbf936c7
Edward T. Kaszubski <ekaszubski@gmail.com>
2016-11-16 19:58:01 -0800

    Add documentation to nodes.

--- nodes/example_deictic_targets_node.cpp
+++ nodes/example_deictic_targets_node.cpp
@@ -1,41 +1,57 @@
 #include <ros/ros.h>
 #include <semio_msgs_ros/DeicticTargets.h>
-#include <semio_msgs_ros/Humanoids.h>
 #include <semio/recognition/deictic_recognizer.h>
 #include <semio/ros/humanoid_source_adapter.h>
 #include <semio/ros/humanoid_sink_adapter.h>
 #include <semio/recognition/humanoid_source_virtual.h>
 
+//! Stores a partial pose (head/neck/torso rotations and the torso position) of a virtual humanoid
 struct TestPose
 {
+    //! Rotation of the head joint for the test pose (relative to global frame)
     Eigen::Vector3d head_rotation;
+    //! Rotation of the neck joint for the test pose (relative to global frame)
     Eigen::Vector3d neck_rotation;
+    //! Rotation of the torso joint for the test pose (relative to global frame)
     Eigen::Vector3d torso_rotation;
+    //! Position of the torso joint for the test pose (relative to global frame)
     Eigen::Vector3d position;
 };
 
+//! Example node demonstrating how to publish targets for deixis recognition
 class ExampleDeicticTargetsNode
 {
 protected:
+    //! ROS message for a vector of deictic targets
     typedef semio_msgs_ros::DeicticTargets _DeicticTargetsMsg;
+    //! ROS message for a single deictic target
     typedef semio_msgs_ros::DeicticTarget _DeicticTargetMsg;
-    typedef semio_msgs_ros::Humanoids _HumanoidsMsg;
-    typedef semio_msgs_ros::Humanoid _HumanoidMsg;
-    typedef semio_msgs_ros::HumanoidJoint _HumanoidJointMsg;
 
+    //! NodeHandle used to interface with ROS
     ros::NodeHandle & _nh_rel;
+    //! Publisher for our deictic targets
     ros::Publisher _deictic_targets_pub;
 
+    //! Pointer to the input source for humanoids
     semio::HumanoidSink::Ptr _humanoid_sink_ptr;
+    //! Pointer to the output sink for humanoids
     semio::HumanoidSource::Ptr _humanoid_source_ptr;
 
+    //! Whether the input source is a semio::HumanoidSourceVirtual
     bool _is_virtual_source;
 
+    //! The list of deictic targets to publish
     semio::DeicticTargetArray _deictic_targets;
 
+    //! The list of test poses to use if #_is_virtual_source == true
     std::vector<TestPose> _test_poses;
 
 public:
+    /**
+    @param nh_rel @copybrief _nh_rel
+    @param humanoid_source_ptr @copybrief _humanoid_source_ptr
+    @param humanoid_sink_ptr @copybrief _humanoid_sink_ptr
+    */
     ExampleDeicticTargetsNode( ros::NodeHandle & nh_rel, semio::HumanoidSource::Ptr humanoid_source_ptr, semio::HumanoidSink::Ptr humanoid_sink_ptr )
     :
         _nh_rel( nh_rel ),
@@ -107,9 +123,10 @@ public:
         }
     }
 
+    //! Publish all relevant data
     void publishData()
     {
-        // publish humanoids
+        //! - Publish humanoids from our input source to our output sink
         _humanoid_sink_ptr->publish( _humanoid_source_ptr->update() );
 
         // publish deictic targets
@@ -117,6 +134,8 @@ public:
 
         deictic_targets_msg.targets.reserve( _deictic_targets.size() );
 
+        //----------
+        //! - Convert deictic targets to ROS message
         for( auto const & deictic_target : _deictic_targets )
         {
             _DeicticTargetMsg deictic_target_msg;
@@ -128,10 +147,18 @@ public:
 
             deictic_targets_msg.targets.push_back( std::move( deictic_target_msg ) );
         }
+        //----------
 
+        //! - Publish deictic targets
         _deictic_targets_pub.publish( deictic_targets_msg );
     }
 
+    //! Main loop
+    /**
+    - If #_is_virtual_source == true
+      - Update our humanoid source with the next test pose
+    - publishData()
+    */
     void spin()
     {
         if( _is_virtual_source )
@@ -190,12 +217,17 @@ public:
 int main( int argc, char ** argv )
 {
     ros::init( argc, argv, "example_deictic_targets_node" );
+    //! - Create NodeHandle with relative namespace
     ros::NodeHandle nh_rel( "~" );
 
+    //! - Create semio::ros::HumanoidSourceAdapter; default to virtual source
     semio::ros::HumanoidSourceAdapter humanoid_source_adapter( nh_rel, "virtual" );
+    //! - Create semio::ros::HumanoidSinkAdapter; default to ROS source
     semio::ros::HumanoidSinkAdapter humanoid_sink_adapter( nh_rel, "ros" );
 
+    //! - Create ExampleDeicticTargetsNode; pass node handle, humanoid source, and humanoid sink
     ExampleDeicticTargetsNode example_deictic_targets_node( nh_rel, humanoid_source_adapter.getHumanoidSource(), humanoid_sink_adapter.getHumanoidSink() );
+    //! - Start main loop ExampleDeicticTargetsNode::spin()
     example_deictic_targets_node.spin();
 
     return 0;
--- nodes/semio_deixis_node.cpp
+++ nodes/semio_deixis_node.cpp
@@ -6,23 +6,39 @@
 #include <semio/recognition/deictic_recognizer.h>
 #include <semio/ros/humanoid_source_adapter.h>
 
+//! Simple ROS wrapper around semio::DeicticRecognizer
 class SemioDeixisNode
 {
 public:
+    //! ROS message for the result of deictic recognition for all humanoids
     typedef semio_msgs_ros::DeicticRecognitionResult _DeicticRecognitionResultMsg;
+    //! ROS message for the result of deictic recognition for a single humanoid
     typedef semio_msgs_ros::DeicticRecognitionHumanoidItem _DeicticRecognitionHumanoidItemMsg;
+    //! ROS message for the result of deictic recognition for a single humanoid joint
     typedef semio_msgs_ros::DeicticRecognitionSourceItem _DeicticRecognitionSourceItemMsg;
+    //! ROS message for the result of deictic recognition for a single deictic target
     typedef semio_msgs_ros::DeicticRecognitionTopNItem _DeicticRecognitionTopNItemMsg;
+    //! ROS message for a vector of deictic targets
     typedef semio_msgs_ros::DeicticTargets _DeicticTargetsMsg;
+    //! ROS message for a single deictic target
     typedef semio_msgs_ros::DeicticTarget _DeicticTargetMsg;
 
+    //! NodeHandle copy used to interface with ROS
     ros::NodeHandle nh_rel_;
+    //! Deictic recognition result publisher
     ros::Publisher result_pub_;
+    //! Deictic targets subscriber
     ros::Subscriber targets_sub_;
 
+    //! Pointer to the input source for humanoids
     semio::HumanoidSource::Ptr humanoid_source_ptr_;
+    //! Semio deictic recognizer
     semio::DeicticRecognizer deictic_recognizer_;
 
+    /**
+    @param nh_rel @copybrief nh_rel_
+    @param humanoid_source_ptr @copybrief humanoid_source_ptr_
+    */
     SemioDeixisNode( ros::NodeHandle & nh_rel, semio::HumanoidSource::Ptr humanoid_source_ptr )
     :
         nh_rel_( nh_rel ),
@@ -47,17 +63,23 @@ public:
         }
     }
 
+    //! Main loop
     void spin()
     {
         ros::Rate loop_rate( 30 );
 
         while( ros::ok() )
         {
+            //! - Trigger ROS callbacks
             ros::spinOnce();
 
+            //! - Pass humanoids to deictic recognizer
             deictic_recognizer_.getHumanoids() = humanoid_source_ptr_->update();
+            //! - Calculate deictic recognition result
             semio::DeicticRecognitionResult const & result( deictic_recognizer_.calculateResult() );
 
+            //----------
+            //! - Convert deictic recognition result to ROS message
             _DeicticRecognitionResultMsg result_msg;
 
             result_msg.humanoids.reserve( result.size() );
@@ -92,17 +114,25 @@ public:
 
                 result_msg.humanoids.push_back( std::move( humanoid_msg ) );
             }
+            //----------
 
+            //! - Publish deictic recognition result
             result_pub_.publish( std::move( result_msg ) );
 
             loop_rate.sleep();
         }
     }
 
+    //! ROS callback for deictic targets
+    /**
+    @param msg_ptr ConstPtr to the deictic target message
+    */
     void targetsCB( _DeicticTargetsMsg::ConstPtr const & msg_ptr )
     {
         auto const & targets_msg = *msg_ptr;
 
+        //----------
+        //! - Convert deictic target message to semio::DeicticTargetArray
         semio::DeicticTargetArray deictic_targets;
 
         for( auto const & target_msg : targets_msg.targets )
@@ -112,7 +142,9 @@ public:
 
             deictic_targets.emplace( name, Eigen::Vector3d( position.x, position.y, position.z ) );
         }
+        //----------
 
+        //! - Update deictic recognizer's list of targets
         deictic_recognizer_.getTargets() = deictic_targets;
     }
 };
@@ -120,11 +152,15 @@ public:
 int main( int argc, char ** argv )
 {
     ros::init( argc, argv, "semio_deixis_node" );
+    //! - Create NodeHandle with relative namespace
     ros::NodeHandle nh_rel( "~" );
 
+    //! - Create semio::ros::HumanoidSourceAdapter
     semio::ros::HumanoidSourceAdapter humanoid_source_adapter( nh_rel );
 
+    //! - Create SemioDeixisNode; pass node handle and humanoid source
     SemioDeixisNode semio_deixis_node( nh_rel, humanoid_source_adapter.getHumanoidSource() );
+    //! - Start main loop SemioDeixisNode::spin()
     semio_deixis_node.spin();
 
     return 0;
